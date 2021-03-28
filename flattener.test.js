e03a5d9c3d9d0ec7f3f53980f160716835d4ba69
Andres <xxdrews@gmail.com>
2021-03-28 20:58:28 +0100

    JSON objects to test are now into their own file, testCases.js

--- flattener.test.js
+++ flattener.test.js
@@ -1,16 +1,27 @@
-const flattener = require("./flattener")
+const flattener = require("./flattener");
+const {JSON1, JSON1Flattened, JSON2, JSON2Flattened, JSON3, JSON3Flattened, JSON4, JSON4Flattened, JSON5, JSON5Flattened} = require("./testCases");
 
-var example = {
-    "a": 1,
-    "b": true,
-    "c": {
-        "d": 3,
-        "e": "test"
-    }
-}
-var exampleFlattened = { "a" : 1, "b": true, "c.d": 3, "c.e": 'test' }
-
-test('example version', ()=>{
-    expect(flattener(example)).toStrictEqual(exampleFlattened)
+//Test 1 
+test('Test 1', () => {
+    expect(flattener(JSON1)).toStrictEqual(JSON1Flattened)
 })
 
+//Test 2
+test('Test 2', () => {
+    expect(flattener(JSON2)).toStrictEqual(JSON2Flattened)
+})
+
+//Test 3
+test('Test 3', () => {
+    expect(flattener(JSON3)).toStrictEqual(JSON3Flattened)
+})
+
+//Test 4
+test('Test 4', () => {
+    expect(flattener(JSON4)).toStrictEqual(JSON4Flattened)
+})
+
+//Test 5
+test('Test 5', () => {
+    expect(flattener(JSON5)).toStrictEqual(JSON5Flattened)
+})
