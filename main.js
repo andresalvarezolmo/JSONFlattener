d5f030f3e144b2707cee69dbdc03af77f6c11a98
Andres <xxdrews@gmail.com>
2021-03-28 19:53:57 +0100

    create separate file to improve runnability and testing

---
+++ main.js
@@ -0,0 +1,10 @@
+const flattener = require("./flattener");
+// npm package to get stdin as a string coming from the pipe
+const getStdin = require('get-stdin');
+
+// async arrow function to be able to use getStdin() which uses await
+(async () => {
+  // store test.json into example variable after parsing it from string to JSON
+  var example = JSON.parse(await getStdin())
+  console.log(flattener(example))
+})()
